8fd4262612799c4a26db2f70fa6fed781ae4c9da
kajame <zmodoact@gmail.com>
2022-09-15 05:11:47 -0700

    work

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-*main: prints strings in the function
-*description: use the main function
-*this program prints:  "with proper grammer , but the outcome is a piece of art"
+*main - prints string in the function
+*description - using the main function
+*this program print "with proper grammer but the outcome is a piece of  art"
 *return 0
 */
 int main(void)
 {
-	print ("with proper grammer, but the outcome is a piece of art,\n");
+	printf (with proper grammer, but the outcome is a piece of art\n")
+	printf ("with proper grammer, but the outcome is a piece of art\n");
 	return (0);
 }
